7407dd942ff0ae2471cc7d930ce1522e434f24be
Riley <91571914+rilstrats@users.noreply.github.com>
2025-11-01 21:11:41 -0600

    completed initial version of script
    - switched to berkely mirror
    - cache sudo password at beginning
    - create domain AKA vm
    - take snapshots before and after edits
    - edit vm
    	- create and autoattach cyber share
    	- copy resize and mountcyber scripts

--- kali-qemu.sh
+++ kali-qemu.sh
@@ -1,12 +1,19 @@
 #!/bin/env bash
-DIR="/var/lib/libvirt/images/"
+POOL="/var/lib/libvirt/images"
+DL_PREFIX="$HOME/Downloads"
 
 validate_version() {
 	if [[ ! $1 =~ 20[0-9]{2}\.[1-4] ]]; then
 		echo "Invalid VERSION: \"$VERSION\""
 		exit 1
 	fi
+	DOMAIN="kali-$VERSION"
+	DL_DIR="$DL_PREFIX/$DOMAIN"
+	URL="https://mirrors.ocf.berkeley.edu/kali-images/kali-$VERSION"
+	ZIP="kali-linux-$VERSION-qemu-amd64.7z"
 	IMAGE="kali-linux-$VERSION-qemu-amd64.qcow2"
+	IMAGE_MOD="$DOMAIN.qcow2"
+	IMAGE_PATH="$POOL/$IMAGE_MOD"
 }
 
 get_version() {
@@ -44,65 +51,81 @@ downgrade_version() {
 # }
 
 fetch_zip() {
-	echo "Attempting to download Kali $VERSION"
-	ZIP="kali-linux-$VERSION-qemu-amd64.7z"
-	if [[ -f $ZIP ]]; then
+	if [[ -f "$DL_DIR/$ZIP" ]]; then
 		return
 	fi
 
-	URL="https://mirrors.ocf.berkeley.edu/kali-images/kali-$VERSION"
-	cd "$HOME/Downloads"
-	wget -q --show-progress "$URL/$ZIP"
-	cd $OLDPWD
+	# echo "Attempting to download Kali $VERSION"
+	wget -qP $DL_DIR --force-progress "$URL/$ZIP"
 	return $?
 }
 
 extract_zip() {
-	if [[ -f $IMAGE ]]; then
+	if [[ -f "$DL_DIR/$IMAGE" ]]; then
 		return
 	fi
-	7z x $ZIP
+	7z x "-o$DL_DIR" "$DL_DIR/$ZIP"
 }
 
-check_image_in_dir() {
-	if [[ -f "$DIR/$IMAGE" ]]; then
-		echo "$IMAGE is already in $DIR"
+check_image_in_pool() {
+	if [[ -f "$IMAGE_PATH" ]]; then
+		echo "Kali $VERSION already exists: $IMAGE_PATH"
 		return
 	fi
 
 	return 1
 }
 
-copy_image_to_dir() {
-	sudo rsync -az --progress $IMAGE $DIR
+copy_image_to_pool() {
+	sudo rsync -az --progress "$DL_DIR/$IMAGE" "$IMAGE_PATH"
 }
 
 fetch_image() {
 	for i in {0..1}; do
-		check_image_in_dir && break
+		check_image_in_pool && break
 		fetch_zip || {
 			downgrade_version
 			continue
 		}
 		extract_zip
-		copy_image_to_dir
+		copy_image_to_pool
 	done
 }
 
 cache_sudo() {
-	echo "sudo will be needed to modify $DIR"
-	sudo -v
+	sudo -nv 2> /dev/null || {
+		echo "sudo password required for interacting with qemu"
+		sudo -v
+	}
 }
 
-# edit_image() {}
+edit_image() {
+	FSTAB_LINE="cyber /cyber virtiofs defaults,nofail 0 0"
+	virt-customize -a $IMAGE_PATH \
+		--mkdir /cyber \
+		--chmod 1777:/cyber \
+		--run-command "grep -qF \"$FSTAB_LINE\" /etc/fstab || echo \"$FSTAB_LINE\" >> /etc/fstab" \
+		--copy-in resize:/usr/local/bin \
+		--copy-in mountcyber:/usr/local/bin
+}
 
-# create_vm() {}
+create_vm() {
+	sed "s/kali-20XX.X/$DOMAIN/g" kali.xml > "$DL_DIR/$DOMAIN.xml"
+	sudo virsh define $DL_DIR/$DOMAIN.xml
+}
+
+snapshot_vm() {
+	sudo virsh snapshot-create-as --domain $DOMAIN --name "$1"
+}
 
 main() {
+	cache_sudo
 	get_version
 	fetch_image
-	# edit_image
-	# create_vm
+	create_vm
+	snapshot_vm original
+	edit_image
+	snapshot_vm custom
 }
 
 main "$@"
